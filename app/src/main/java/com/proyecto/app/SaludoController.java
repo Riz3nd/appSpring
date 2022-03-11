212fd69f0caf26dedc9e65142d0cc68936644373
SCIS3-PC19\SCIS3-19 <q@q.com>
2022-03-10 19:51:16 -0500

    Tercer Commit
    * Se añadieron nuevas clases para crear una mini Api, esta devuelve un JSON.

---
+++ app/src/main/java/com/proyecto/app/SaludoController.java
@@ -0,0 +1,21 @@
+package com.proyecto.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class SaludoController {
+    private static final String plantilla = "Hola, %s!";
+    private final AtomicLong contador = new AtomicLong();
+
+    @GetMapping("/saludo")
+    public Saludo verSaludo(@RequestParam(value = "nombre",defaultValue = "N/D") String nombre){
+        return new Saludo(contador.incrementAndGet(),String.format(plantilla,nombre));
+    }
+
+
+
+}
